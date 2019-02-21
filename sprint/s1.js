603c5ee1e058d01c7dc61dc54fdb53237140d7a9
Iavich <44722329+Iavich@users.noreply.github.com>
2019-02-21 11:31:54 +0100

    version nouvelle de mon portofolio

--- sprint/s1.js
+++ sprint/s1.js
@@ -6,6 +6,4 @@ console.log("okacode");
 
 
 
-// s2 Affiher okacode 22 fois  
-let chorus = 'okacode,';
-console.log('affiche okacode 22 fois": '+ chorus.repeat(22)); //utilisation d'une methode repeat
+
--- sprint/s2.js
+++ sprint/s2.js
@@ -1,3 +1,6 @@
 // s2 Affiher okacode 22 fois  
 let chorus = 'okacode,';
 console.log('affiche okacode 22 fois": '+ chorus.repeat(22)); //utilisation d'une methode repeat
+
+
+//

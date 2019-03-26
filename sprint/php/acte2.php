1e76ef18b4f5385d07f31db4b09cc9059547a5ad
Iavich <44722329+Iavich@users.noreply.github.com>
2019-03-26 15:26:14 +0100

    ajout de acte 2

---
+++ sprint/php/acte2.php
@@ -0,0 +1,12 @@
+<!-- Les types de variables en php -->
+<?php
+
+$var ='jean';
+$Var ='paul';  
+$decimal= 1.45;
+$age_du_client =30;
+echo "$var, <br/> $Var,  <br/>";  // affiche Paul et jean et un decimal
+echo "la taille de paul est $decimal metres <br/>";
+echo "Le directeur a $age_du_client ans";
+?>
+

fc85689532d7b7027bb48b9f32f3954d41113a8d
Iavich <44722329+Iavich@users.noreply.github.com>
2019-03-27 10:24:25 +0100

    ajout d;un fichier acte3.php

---
+++ sprint/php/acte3.php
@@ -0,0 +1,13 @@
+<?php
+$professeur = array("Jean", "Sebastian", "kevin", "Aude" , "Audile", "Rodrigue", "Pierre", "grace", "Joe", "Aude");
+echo $professeur[0]. '<br>';
+echo $professeur[1]. '<br>';
+echo $professeur[2]. '<br>';
+echo $professeur[3]. '<br>';
+echo $professeur[4]. '<br>';
+echo $professeur[5]. '<br>';
+echo $professeur[6]. '<br>';
+echo $professeur[7]. '<br>';
+echo $professeur[8]. '<br>';
+echo $professeur[9].'<br>';
+?>
